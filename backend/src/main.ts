23b8102b4e57be5a7e3e695deae59177b9e27d34
v-chern <vladimir.v.chernikov@yandex.ru>
2025-11-01 19:48:26 +0100

    unit tests for services and controllers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import { TskvLogger } from './logger/tskv.logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true
+  });
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useLogger(new TskvLogger());
   await app.listen(3000);
 }
 bootstrap();
--- backend/src/order/order.controller.spec.ts
+++ backend/src/order/order.controller.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import { promiseHooks } from 'v8';
 
 describe('OrderController', () => {
   let controller: OrderController;
+  let orderService: OrderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrderController],
-    }).compile();
+      providers: [OrderService]
+    })
+    .overrideProvider(OrderService)
+    .useValue({
+      createOrder: jest.fn(),
+    })
+    .compile();
 
     controller = module.get<OrderController>(OrderController);
+    orderService = module.get<OrderService>(OrderService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call createOrder from OrderService', async () => {
+    const orderData = { 
+      email: 'test@test.ts',
+      phone: '1234567890',
+      tickets: []
+    };
+    await controller.createOrder(orderData);
+    expect(orderService.createOrder).toHaveBeenCalledWith(orderData);
   });
 });
