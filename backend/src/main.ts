406827d57f759a8536b0968b9aa46f6c7e4e10d6
v-chern <vladimir.v.chernikov@yandex.ru>
2025-11-09 13:16:09 +0100

    properties definitions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,10 @@ async function bootstrap() {
   });
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(new ValidationPipe({ 
+    transform: true, //transform payload to DTO class
+    whitelist: true //exclude properties from payload which are not in DTO
+   }));
   app.useLogger(new DevLogger());
   await app.listen(3000);
 }
